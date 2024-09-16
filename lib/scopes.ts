c967cebfc11aa26bb3d63a75b9924b9e9f9c3b70
William Danielsson <william.danielsson@knightec.se>
2024-09-16 19:44:14 +0200

    Added azure provider

--- lib/scopes.ts
+++ lib/scopes.ts
@@ -3,4 +3,5 @@ import { Provider } from "./config";
 export const scopes: Record<Provider, string> = {
   github: "repo,user",
   gitlab: "api",
+  azure: "https://graph.microsoft.com/.default",
 };
